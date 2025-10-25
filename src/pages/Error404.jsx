54e80b04295ef37492fce198e7e830092b803f54
armadne <madane.aroul@hotmail.com>
2025-10-25 23:15:42 +0200

    création page erreur 404

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,3 +1,17 @@
+import { Link } from "react-router-dom";
+import "../styles/Error404.scss";
+import image404 from "../assets/404.png";
+
 export default function Error404() {
-  return <h1>Erreur 404 – Page non trouvée</h1>;
+  return (
+    <main className="error404">
+      <img src={image404} alt="Erreur 404" className="error404__img" />
+      <h2 className="error404__text">
+        Oups ! La page que vous demandez n'existe pas.
+      </h2>
+      <Link to="/" className="error404__link">
+        Retourner sur la page d’accueil
+      </Link>
+    </main>
+  );
 }
