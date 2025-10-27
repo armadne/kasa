bf9290aaccd66752798114fdb7e5d5445c4b56e6
armadne <madane.aroul@hotmail.com>
2025-10-27 23:15:13 +0100

    mise en place des chevrons pour collapse dans la page about

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
+import arrowDown from "../assets/arrow-down.png";
+import arrowUp from "../assets/arrow-up.png";
+
 
 function Collapse({ title, content }) {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="collapse">
+    <div className={`collapse ${open ? "open" : ""}`}>
       <div className="collapse-header" onClick={() => setOpen(!open)}>
         <h3>{title}</h3>
-        <span>{open ? "▲" : "▼"}</span>
+        <img
+          src={open ? arrowUp : arrowDown}
+          alt={open ? "Flèche vers le haut" : "Flèche vers le bas"}
+          className="collapse-arrow"
+        />
       </div>
       {open && <div className="collapse-content">{content}</div>}
     </div>
